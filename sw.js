79eed4ed0d269d02998b861cc197bfae7980b27f
gds3265 <elodie.jegou.gds65@reseaugds.com>
2026-08-20 15:53:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const C='audit-ovin-v1.9';
+const C='audit-ovin-v2.1';
 const PREFIX='audit-ovin-';
 const BASE='/Audit-Ovin-gds-32-65/';
 const STATIC=[
-  BASE,BASE+'index.html',BASE+'manifest.webmanifest?v=1.9',
-  BASE+'icon-192.png?v=1.9',BASE+'icon-512.png?v=1.9',
-  BASE+'icon-maskable-512.png?v=1.9',BASE+'apple-touch-icon.png?v=1.9'
+  BASE,BASE+'index.html',BASE+'manifest.webmanifest?v=2.1',
+  BASE+'icon-192.png?v=2.1',BASE+'icon-512.png?v=2.1',
+  BASE+'icon-maskable-512.png?v=2.1',BASE+'apple-touch-icon.png?v=2.1'
 ];
 self.addEventListener('install',e=>{
   self.skipWaiting();
